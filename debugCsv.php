da3674d92bd7f063d83f76f8db9c959dd0637305
asaph <ace@dontletgo.com>
2014-04-17 14:55:15 -0700

    If CSV header has more columns than data row, pad the data row with enough columns so that we can still combine the headers with the data and output something readable

--- debugCsv.php
+++ debugCsv.php
@@ -22,9 +22,15 @@ for ($j=1; $j<$argc; $j++) {
 	$lines = preg_split('/[\r\n]+/', $csv, -1, PREG_SPLIT_NO_EMPTY);
 	if (!empty($lines)) {
 		$headers = str_getcsv($lines[0], $delimiter);
+		$headerColumnCount = count($headers);
 		for ($i=1, $length=count($lines); $i<$length; $i++) {
 			$row = str_getcsv($lines[$i], $delimiter);
-			if (count($headers) == count($row)) {
+			$rowColumnCount = count($row);
+			while ($rowColumnCount < $headerColumnCount) {
+				$row[] = '';
+				$rowColumnCount++;
+			}
+			if ($headerColumnCount == $rowColumnCount) {
 				$row = array_combine($headers, $row);
 			}
 			print_r($row);
